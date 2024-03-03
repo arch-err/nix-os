c57b79799b21733c6d707abe2a1a4be1568c9304
arch-err <archer@jesber.xyz>
2024-03-03 12:37:05 +0100

    Added rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+HOST="default"
+
+set -e
+#pushd "~/.os/hosts/$HOST/"
+cd  "$HOME/.os/hosts/$HOST/"
+"$EDITOR" configuration.nix
+# alejandra . &>/dev/null
+#git diff -U0 *.nix
+echo "NixOS Rebuilding..."
+sudo nixos-rebuild switch --flake "/etc/nixos/nix-os#$HOST" &>nixos-switch.log || (
+ cat nixos-switch.log | grep --color error && false)
+gen=$(nixos-rebuild --flake "/etc/nixos/nix-os#$HOST" list-generations | grep current)
+git commit -am "$gen"
+cd - 
+# popd
