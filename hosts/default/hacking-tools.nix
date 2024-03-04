9c84fd85ac9d86c1bc5feafcce8ff60cdf6fb855
arch-err <archer@jesber.xyz>
2024-03-04 12:23:44 +0100

    114 current  2024-03-04 12:23:39  24.05.20240229.1536926   6.6.18                          *

--- hosts/default/hacking-tools.nix
+++ hosts/default/hacking-tools.nix
@@ -562,29 +562,29 @@
       xcrawl3r
       xsubfind3r
 
-      # # Microsoft infrastructure and Windows-related tools, incl. SMB
-      # bloodhound-py
-      # chainsaw
-      # certipy
-      # certsync
-      # coercer
-      # enum4linux
-      # enum4linux-ng
-      # erosmb
-      # evil-winrm
-      # go365
-      # gomapenum
-      # kerbrute
-      # knowsmore
-      # lil-pwny
-      # nbtscan
-      # nbtscanner
-      # offensive-azure
-      # python3Packages.lsassy
-      # python3Packages.pypykatz
-      # samba
-      # smbmap
-      # smbscan
+      # Microsoft infrastructure and Windows-related tools, incl. SMB
+      bloodhound-py
+      chainsaw
+      certipy
+      certsync
+      coercer
+      enum4linux
+      enum4linux-ng
+      erosmb
+      evil-winrm
+      go365
+      gomapenum
+      kerbrute
+      knowsmore
+      lil-pwny
+      nbtscan
+      nbtscanner
+      offensive-azure
+      python3Packages.lsassy
+      python3Packages.pypykatz
+      samba
+      smbmap
+      smbscan
 
       # # Wireless tools
       # aircrack-ng
