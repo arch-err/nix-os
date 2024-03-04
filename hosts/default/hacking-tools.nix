a279288f261cfd5d57901d961e1d350713e2bfea
arch-err <archer@jesber.xyz>
2024-03-04 11:14:13 +0100

    90 current  2024-03-04 11:14:09  24.05.20240229.1536926   6.6.18                          *

--- hosts/default/hacking-tools.nix
+++ hosts/default/hacking-tools.nix
@@ -104,49 +104,49 @@
       log4j-vuln-scanner
       logmap
 
-      # # Forensic tools
-      # afflib
-      # amoco
-      # acquire
-      # dcfldd
-      # ddrescue
-      # dislocker
-      # dismember
-      # exiv2
-      # ext4magic
-      # extundelete
-      # foremost
-      # gef
-      # gzrt
-      # hivex
-      # hstsparser
-      # noseyparker
-      # ntfs3g
-      # ntfsprogs
-      # nwipe
-      # recoverjpeg
-      # safecopy
-      # sleuthkit
-      # srm
-      # stegseek
-      # testdisk
-      # volatility3
-      # wipe
-      # xorex
-      # 
-      # # Fuzzing tools
-      # afl
-      # aflplusplus
-      # feroxbuster
-      # ffuf
-      # gobuster
-      # honggfuzz
-      # radamsa
-      # regexploit
-      # scout
-      # ssdeep
-      # wfuzz
-      # zzuf
+      # Forensic tools
+      afflib
+      amoco
+      acquire
+      dcfldd
+      ddrescue
+      dislocker
+      dismember
+      exiv2
+      ext4magic
+      extundelete
+      foremost
+      gef
+      gzrt
+      hivex
+      hstsparser
+      noseyparker
+      ntfs3g
+      ntfsprogs
+      nwipe
+      recoverjpeg
+      safecopy
+      sleuthkit
+      srm
+      stegseek
+      testdisk
+      volatility3
+      wipe
+      xorex
+      
+      # Fuzzing tools
+      #afl
+      #aflplusplus
+      #feroxbuster
+      #ffuf
+      #gobuster
+      #honggfuzz
+      #radamsa
+      #regexploit
+      #scout
+      #ssdeep
+      #wfuzz
+      #zzuf
 
       # # Generic tools (terminals, packers, clients, etc.)
       # chrony
