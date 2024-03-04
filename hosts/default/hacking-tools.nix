1df46f08d7cd82eb61445ae8d904fce81f10e3db
arch-err <archer@jesber.xyz>
2024-03-04 12:14:50 +0100

    107 current  2024-03-04 12:14:45  24.05.20240229.1536926   6.6.18                          *

--- hosts/default/hacking-tools.nix
+++ hosts/default/hacking-tools.nix
@@ -403,52 +403,52 @@
       rshijack
       zap
 
-      # # Tools for testing various services (SSH, SNMP, etc.)
-      # acltoolkit
-      # checkip
-      # ghunt
-      # ike-scan
-      # keepwn
-      # metasploit
-      # nbutools
-      # nuclei
-      # openrisk
-      # osv-scanner
-      # uncover
-      # traitor
-      # # E-Mail
-      # mx-takeover
-      # ruler
-      # swaks
-      # trustymail
-      # # Databases
-      # ghauri
-      # mongoaudit
-      # pysqlrecon
-      # sqlmap
-      # # SNMP
-      # braa
-      # onesixtyone
-      # snmpcheck
-      # # SSH
-      # baboossh
-      # sshchecker
-      # ssh-audit
-      # ssh-mitm
-      # # IDS/IPS/WAF
-      # teler
-      # waf-tester
-      # wafw00f
-      # # CI
-      # oshka
-      # # Terraform
-      # terrascan
-      # tfsec
-      # # Supply chain
-      # chain-bench
-      # witness
-      # # WebDAV
-      # davtest
+      # Tools for testing various services (SSH, SNMP, etc.)
+      acltoolkit
+      checkip
+      ghunt
+      ike-scan
+      keepwn
+      metasploit
+      nbutools
+      nuclei
+      openrisk
+      osv-scanner
+      uncover
+      traitor
+      # E-Mail
+      mx-takeover
+      ruler
+      swaks
+      trustymail
+      # Databases
+      ghauri
+      mongoaudit
+      pysqlrecon
+      sqlmap
+      # SNMP
+      braa
+      onesixtyone
+      snmpcheck
+      # SSH
+      baboossh
+      sshchecker
+      ssh-audit
+      ssh-mitm
+      # IDS/IPS/WAF
+      teler
+      waf-tester
+      wafw00f
+      # CI
+      oshka
+      # Terraform
+      terrascan
+      tfsec
+      # Supply chain
+      chain-bench
+      witness
+      # WebDAV
+      davtest
 
       # # Smartcard tools
       # cardpeek
