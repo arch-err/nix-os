d949029529676d6626bd77b89c15a69feb3cd9dd
arch-err <archer@jesber.xyz>
2024-03-09 16:13:50 +0100

    131 current  2024-03-09 16:04:24  24.05.20240229.1536926   6.6.18                          *

--- rebuild.sh
+++ rebuild.sh
@@ -7,32 +7,33 @@ BUILD_CMD="nixos-rebuild switch --flake "/etc/nixos/nix-os#$HOST""
 LOGFILE="nixos-switch.log"
 test -z $EDITOR && EDITOR="vim"
 
-cd ~/.os/hosts/$HOST
+pushd ~/.os/hosts/$HOST &>/dev/null
 
 
 "$EDITOR" configuration.nix
 
-# if which alejandra &>/dev/null
-# then
-#     alejandra . &>/dev/null
-# fi
-# 
-# git diff -U0 *.nix
-# 
-# echo "NixOS Rebuilding..."
+if which alejandra &>/dev/null
+then
+    alejandra . &>/dev/null
+fi
+
+git diff -U0 *.nix
+
+echo "NixOS Rebuilding..."
 sudo $BUILD_CMD | tee "$LOGFILE" 
 
-# if grep "error" "$LOGFILE"
-# then
-#   echo -en "\e[0;31m"
-#   echo "Errors were found :("
-#   echo -en "\e[0m"
-# fi
+if grep "error" "$LOGFILE"
+then
+  echo -en "\e[0;31m"
+  echo "Errors were found :("
+  echo -en "\e[0m"
+fi
 
-# grep --color error "$LOGFILE" 
+grep --color error "$LOGFILE" 
 
+BUILD_CMD=$(echo "$BUILD_CMD" | sed 's/ switch//')
 gen=$($BUILD_CMD list-generations | grep current)
 
 git commit -am "$gen"
 
-cd - &>/dev/null
+popd
