4c2c2de3d39d57129ecee93aa40b95b54fbddac9
arch-err <archer@jesber.xyz>
2024-03-11 21:43:41 +0100

    143 current  2024-03-11 21:42:23  24.05.20240229.1536926   6.6.18                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -242,7 +242,7 @@
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.support32Bit = true;
+  # hardware.pulseaudio.support32Bit = true;
   nixpkgs.config.pulseaudio = true;
 
   home-manager = {
