90669a4ede1df5d60588d3addc465c409afc4052
arch-err <archer@jesber.xyz>
2024-03-11 21:54:25 +0100

    143 current  2024-03-11 21:42:23  24.05.20240229.1536926   6.6.18                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -325,6 +325,27 @@
     xdg-desktop-portal-hyprland
   ];
 
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+      bluez_monitor.properties = {
+      	["bluez5.enable-sbc-xq"] = true,
+      	["bluez5.enable-msbc"] = true,
+      	["bluez5.enable-hw-volume"] = true,
+      	["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '')
+  ];
+
   users.users.archerr.extraGroups = ["docker"];
   virtualisation.docker.storageDriver = "btrfs";
   virtualisation.docker.enable = true;
