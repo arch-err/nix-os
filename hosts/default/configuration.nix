6ef1c31c1a3a52f27d84607c8520dd3b4b182a6d
arch-err <archer@jesber.xyz>
2024-03-12 08:00:52 +0100

    147 current  2024-03-12 08:00:09  24.05.20240229.1536926   6.6.18                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -375,6 +375,7 @@
     enable = true;
     xwayland.enable = true;
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    systemd.enable = true;
     # portalPackage = inputs.xdg-desktop-portal-hyprland.packages."${pkgs.system}".xdg-desktop-portal-hyprland;
   };
 
