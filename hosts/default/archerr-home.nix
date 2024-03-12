ef54d51c16f2de7cd26850f86c1df09e88ae78a5
arch-err <archer@jesber.xyz>
2024-03-12 08:00:16 +0100

    147 current  2024-03-12 08:00:09  24.05.20240229.1536926   6.6.18                          *

--- hosts/default/archerr-home.nix
+++ hosts/default/archerr-home.nix
@@ -70,6 +70,19 @@
   #
   #  /etc/profiles/per-user/archerr/etc/profile.d/hm-session-vars.sh
   #
+
+  wayland.windowManager.hyprland = {
+    # Whether to enable Hyprland wayland compositor
+    enable = true;
+    # The hyprland package to use
+    package = pkgs.hyprland;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+
+    # Optional
+    # Whether to enable hyprland-session.target on hyprland startup
+    systemd.enable = true;
+  };
   home.sessionVariables = {
     BROWSER = "floorp";
     SEC_BROWSER = "firefox";
